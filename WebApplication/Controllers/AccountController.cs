2d9230ca87681282a4d6eadf6ed0be8320ac7524
HagaiCo <50545614+hagai1994@users.noreply.github.com>
2020-10-26 19:41:07 +0200

    create add file logic

---
+++ WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,191 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Firebase.Auth;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using AuthenticationProperties = Microsoft.Owin.Security.AuthenticationProperties;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private static string ApiKey = "AIzaSyBXcxNJb-mTnFWQYshXyELXZyj14u40xwQ";
+        private static string Bucket = "steganography-5582f.firebaseio.com";
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> SignUp(SignUpModel model)
+        {
+            if (string.IsNullOrEmpty(model.Email))
+                return View();
+
+            try
+            {
+                var auth = new FirebaseAuthProvider(new Firebase.Auth.FirebaseConfig(ApiKey)); 
+                var a = await auth.CreateUserWithEmailAndPasswordAsync(model.Email, model.Password, model.Name, true);
+                ModelState.AddModelError(string.Empty, "Please Verify your email then login.");
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError(string.Empty, e.Message);
+                Console.WriteLine(e);
+            }
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Login(string returnUrl)
+        {
+            try
+            {
+                // Verification.
+                if (User.Identity.IsAuthenticated)
+                {
+                    return this.RedirectToLocal(returnUrl);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Info
+                Console.Write(ex);
+            }
+
+            // Info.
+            return this.View();
+        }
+
+        // GET: Account
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            try
+            {
+                // Verification.
+                if (ModelState.IsValid)
+                {
+                    var auth = new FirebaseAuthProvider(new FirebaseConfig(ApiKey));
+                    var ab = await auth.SignInWithEmailAndPasswordAsync(model.Email, model.Password);
+                    string token = ab.FirebaseToken;
+                    var user = ab.User;
+                    if (token != "")
+                    {
+
+                        SignInUser(user.Email, token, false);
+                        return RedirectToLocal(returnUrl);
+
+                    }
+                    else
+                    {
+                        // Setting.
+                        ModelState.AddModelError(string.Empty, "Invalid username or password.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Info
+                Console.Write(ex);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return this.View(model);
+        }
+
+        private void SignInUser(string email, string token, bool isPersistent)
+        {
+            // Initialization.
+            var claims = new List<Claim>();
+
+            try
+            {
+                // Setting
+                claims.Add(new Claim(ClaimTypes.Email, email));
+                claims.Add(new Claim(ClaimTypes.Authentication, token));
+                var claimIdenties = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+                var ctx = HttpContext.AuthenticateAsync();
+                var authenticationManager = ctx.Authentication;
+                // Sign In.
+                ctx.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, claimIdenties);
+            }
+            catch (Exception ex)
+            {
+                // Info
+                throw ex;
+            }
+        }
+
+        private void ClaimIdentities(string userName, string isPersistent)
+        {
+            //Initialization
+            var claims = new List<Claim>();
+            try
+            {
+                //Setting
+                claims.Add(new Claim(ClaimTypes.Name, userName));
+                var claimIdentities = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            try
+            {
+                //Verification
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    //info
+                    return Redirect(returnUrl);
+                }
+
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            //info
+            return this.RedirectToAction("LogOut", "Account");
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            if (ctx.Authentication.User == null)
+            {
+                
+            }
+            var authenticationManager = ctx.Authentication;
+            authenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Login", "Account");
+        }
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,19 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services;
+using Firebase.Auth;
+using Firebase.Storage.Client;
+using Firebase.Storage.Options;
+using FirebaseAdmin.Auth;
+using FireSharp;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.FirebaseModel;
+using WebApplication1.Models;
+using FirebaseAuth = Firebase.Auth.FirebaseAuth;
 
 namespace WebApplication1.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private static string ApiKey = "AIzaSyBXcxNJb-mTnFWQYshXyELXZyj14u40xwQ";
+        private static string Bucket = "steganography-5582f.appspot.com";
+        private static string adminEmail = "hagai729@gmail.com";
+        private static string adminPass = "123123";
+        static IFirebaseConfig config = new FireSharp.Config.FirebaseConfig()
+        {
+            AuthSecret = "jnUAPjDAutWRVPuwEO4QcRJIj1bsmZc8plnSrM9K",
+            BasePath = "https://steganography-5582f.firebaseio.com/"
+        };
+        FirebaseClient _client = new FirebaseClient(config);
+
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Index(FileUploadViewModel fileUploadViewModel)
+        {
+            FileStream stream;
+            if(fileUploadViewModel.File.ContentLength > 0)
+            {
+                var path = Path.Combine(Server.MapPath("~/Content/images/"), fileUploadViewModel.File.FileName);
+                fileUploadViewModel.FilePath = path;
+                fileUploadViewModel.File.SaveAs(path);
+                using ( stream = new FileStream(Path.Combine(path), FileMode.Open));
+                Task.Run(() => Upload(fileUploadViewModel, stream)).GetAwaiter();
+            }
+            return View();
+        }
+        public async void Upload(FileUploadViewModel fileUploadViewModel, FileStream stream)
+        {
+            var auth = new FirebaseAuthProvider(new FirebaseConfig(ApiKey));
+            var a = await auth.SignInWithEmailAndPasswordAsync(adminEmail, adminPass);
 
+            var cancellation = new CancellationTokenSource();
+            var firebaseStorageOptions = new FirebaseStorageOptions 
+            {
+                AuthTokenAsync = () => Task.FromResult(a.FirebaseToken),
+                ThrowOnCancel = true     
+            };
+            _client = new FirebaseClient(config);
+            var data = fileUploadViewModel;
+            
+            //todo - create convert method
+            FileUploadFirebaseModel fileUploadFirebaseModel = new FileUploadFirebaseModel();
+            fileUploadFirebaseModel.File = System.IO.File.ReadAllBytes(fileUploadViewModel.FilePath);
+            fileUploadFirebaseModel.Id = data.Id;
+            fileUploadFirebaseModel.FileName = data.File.FileName;
+            fileUploadFirebaseModel.PermittedUsers = data.PermittedUsers;
+
+            PushResponse response = _client.Push("Files/", fileUploadFirebaseModel);
+            fileUploadFirebaseModel.Id = response.Result.name;
+            SetResponse setResponse = _client.Set("Files/" + fileUploadFirebaseModel.Id, fileUploadFirebaseModel);
+            //var task = new FirebaseStorage(Bucket, firebaseStorageOptions).Child("images").Child(fileUploadViewModel.File.FileName).PutAsync(stream, cancellation.Token);
+        
+            try
+            {
+                //await task;
+                ModelState.AddModelError(string.Empty, "Uploaded Successfully");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception was thrown: {ex}");
+            }
+        }
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -24,6 +102,15 @@ namespace WebApplication1.Controllers
         {
             ViewBag.Message = "Your contact page.";
             return View();
+        }
+        
+        [WebMethod]
+        public static IEnumerable<SelectListItem> GetAllUsers()
+        {
+            var list1 = new List<string>(){"hagai729@gmail.com"};
+            var selectListItems = list1.Select(x => new SelectListItem(){ Value = x, Text = x }).ToList();
+
+            return selectListItems.AsEnumerable();
         }
     }
 }
