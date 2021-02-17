871660ced187ab96bd27dbf5a96eb34709d256be
HagaiCo <50545614+hagai1994@users.noreply.github.com>
2021-02-17 19:38:36 +0200

    Improving performance.

--- WebApplication/RequestModel/FileDataUploadRequestModel.cs
+++ WebApplication/RequestModel/FileDataUploadRequestModel.cs
@@ -7,30 +7,33 @@ namespace WebApplication.RequestModel
 {
     public class FileDataUploadRequestModel
     {
-        [Required]
-        [Display(Name = "Choose file to upload")] 
-        public HttpPostedFileBase File { get; set; }
-        
         [Key] 
         public string Id { get; set; }
+        
+        [Required]
+        [Display(Name = "Choose file to upload")] 
+        public HttpPostedFileBase FileAsHttpPostedFileBase { get; set; }
+
+        public byte[] FileAsByteArray { get; set; }
+
+        public string FileName { get; set; }
+
         [Required]
         [Display(Name = "Type text to hide")] 
         public string SecretMessage { get; set; }
+        
         [Required] 
         [Display(Name = "Choose permitted users")] 
         public List<string> PermittedUsers { get; set; }
+        
         public string FilePath { get; set; }
         
         public FileType FileType { get; set; }
         
-        
         [Display(Name="Choose Encryption Method")]
         public EncryptionMethod EncryptionMethod { get; set; }
+        
         [Display(Name="Choose hiding method")]
         public HidingMethod HidingMethod { get; set; }
-        
-     
     }
-    
-    
 }
