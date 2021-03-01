da22560d3b446c7257ab4685a15bdac5487d6f80
Tomer Mamistvalov <mamistom8@hotmail.com>
2021-03-02 00:53:29 +0200

    .

---
+++ WebApplication/Scripts/GetAllAssignedFileData.js
@@ -0,0 +1,18 @@
+﻿const myButton = document.getElementsByClassName('btn1');
+const myText = document.getElementById('txt1');
+
+
+for (const btn of myButton) {
+    btn.addEventListener('click', function(e) {
+        //e.preventDefault(); //This prevents the default action
+        //alert("This is scary"); //Show the alert  
+        //myText.innerHTML="Tomer";
+
+        const temp = myText.className;
+        if (temp == "txt1")
+            myText.className = "txt2";
+        else myText.className = "txt1";
+    });
+
+
+}
