fbeed848600e807cdc7e0e33f72f4901f4f8f525
Tomer Mamistvalov <mamistom8@hotmail.com>
2021-03-02 13:25:23 +0200

    styling fixed

--- WebApplication/Scripts/GetAllAssignedFileData.js
+++ WebApplication/Scripts/GetAllAssignedFileData.js
@@ -1,18 +1,53 @@
 ﻿const myButton = document.getElementsByClassName('btn1');
-const myText = document.getElementById('txt1');
 
 
 for (const btn of myButton) {
     btn.addEventListener('click', function(e) {
-        //e.preventDefault(); //This prevents the default action
+        e.preventDefault(); //This prevents the default action
+        //alert("This is scary"); //Show the alert  
+        //myText.innerHTML="Tomer";
+        
+        const btnID = btn.id;
+        const p = document.getElementById("text "+btnID)
+        console.log(btnID + "text")
+        if (p.className == "txt1")
+            p.className = "txt2";
+        else p.className = "txt1";
+
+    });
+
+}
+
+
+
+/*
+for (const btn of myButton) {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault(); //This prevents the default action
         //alert("This is scary"); //Show the alert  
         //myText.innerHTML="Tomer";
 
-        const temp = myText.className;
+        const temp = myText[1].className;
         if (temp == "txt1")
-            myText.className = "txt2";
-        else myText.className = "txt1";
+            myText[1].className = "txt2";
+        else myText[1].className = "txt1";
+        
     });
-
-
+    
 }
+
+for(let i=0;i<myButton.length;i++){
+    myButton[i].addEventListener('click', function(e) {
+        e.preventDefault(); //This prevents the default action
+        
+
+        const id = myButton[i].id;
+        const txt = document.getElementById(id);
+        if(txt.className == "txt1")
+            txt.className = "txt2";
+        else txt.className = "txt1";
+
+    });
+    
+}*/
+
